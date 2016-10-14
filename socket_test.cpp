6fda1c537614077de7d2b506755131afc886a03e
fwsgonzo <fwsgonzo@hotmail.com>
2016-10-14 13:55:41 +0200

    Socket test using sockaddr_in

--- socket_test.cpp
+++ socket_test.cpp
@@ -3,15 +3,21 @@
 #include <string.h>
 #include <assert.h>
 #include <unistd.h> // close
+#include <stdio.h>
 
 void socket_test()
 {
   /// do POSIX things
   auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  printf("socket() returned %d\n", sockfd);
+
+  //in_addr shit_addr;
+  //inet_aton("10.0.0.1", shit_addr);
 
   struct sockaddr_in serv_addr;
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr   = 16777226;
   serv_addr.sin_port   = 6667;
 
   if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
